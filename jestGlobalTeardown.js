3eb79cc9b466e808fee626b76cdf54c39b2ec14f
jihchi <achi@987.tw>
2020-04-20 16:23:54 +0800

    Use "setupFilesAfterEnv" to invoke bisect to write code coverage

--- jestGlobalTeardown.js
+++
@@ -1,5 +0,0 @@
-const bisectRuntime = require("bisect_ppx/lib/js/src/runtime/bucklescript/runtime.js");
-
-module.exports = function globalTeardown(_globalConfig) {
-  bisectRuntime.write_coverage_data();
-};
